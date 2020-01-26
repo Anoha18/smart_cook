a7b870bbc63113afdee908d4fd48a14abd3bacf1
Vladislav <vlad@kabiev.com>
2020-01-26 23:32:40 +0300

    some update

---
+++ server/middleware/graphql.js
@@ -0,0 +1,13 @@
+const {GraphQLSchema} = require('graphql');
+const graphqlHTTP = require('express-graphql');
+
+const {queryType} = require('../graphQL');
+
+const schema = new GraphQLSchema({ query: queryType });
+
+module.exports = (app) => {
+  app.use('/api', graphqlHTTP({
+    schema,
+    graphiql: true,
+  }))
+};
